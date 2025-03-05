9047e062f66623df1cc942b861d1bcedd5b18ede
lxd <dennis940127@gmail.com>
2025-03-05 18:00:02 +0800

    1

--- test.py
+++ test.py
@@ -3,7 +3,7 @@
 from seleniumbase import SB
 
 with SB(uc=True, test=True, locale_code="en", xvfb="True") as sb:
-    url = "https://www.ip2location.com/"
+    url = "https://ipinfo.thordata.com/"
     sb.uc_open_with_reconnect(url, 5)
     sb.uc_gui_click_captcha()
     html_content = sb.get_page_source()
