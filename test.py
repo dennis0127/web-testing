61ecc6df008bc164f08e17d21bebe23130eefbf6
lxd <dennis940127@gmail.com>
2025-03-26 10:02:41 +0800

    1

--- test.py
+++ test.py
@@ -4,10 +4,3 @@ from seleniumbase import SB
 
 with SB(uc=True, test=True, locale_code="en", xvfb="True") as sb:
     url = "https://www.ygg.re/"
-    # sb.uc_open_with_reconnect(url, 5)
-    sb.activate_cdp_mode(url)
-    sb.uc_gui_click_captcha()
-    html_content = sb.cdp.get_page_source()
-    print(html_content)
-
-# 在这里添加一个换行符  <-- 删除空格或制表符
