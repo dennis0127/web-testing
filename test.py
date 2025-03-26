4245cea2c675d233bdbc8d07e7d481dc3f0072cb
lxd <dennis940127@gmail.com>
2025-03-26 15:49:25 +0800

    1

--- test.py
+++ test.py
@@ -9,10 +9,10 @@ def test():
         # url = "https://vstat.info/amazon.com"
         # url = "www.planetminecraft.com/account/sign_in/"
         url = "https://www.ygg.re/"
-        sb.uc_open_with_reconnect(url, 5)
-        # sb.activate_cdp_mode(url)
+        # sb.uc_open_with_reconnect(url, 5)
+        sb.activate_cdp_mode(url)
         print("-----开始执行测试用例2-----")
-        sb.sleep(3)
+        sb.sleep(5.2)
         sb.uc_gui_click_captcha()
         sb.sleep(2)
         print("-----开始执行测试用例3-----")
