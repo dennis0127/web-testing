b9ca8c058c1eb403a9d539d7d27c5437bc03a889
lxd <dennis940127@gmail.com>
2025-03-26 10:44:06 +0800

    1

--- test.py
+++ test.py
@@ -5,6 +5,7 @@ from seleniumbase import SB
 
 def test():
     with SB(uc=True, test=True, locale_code="en", xvfb="True") as sb:
+        print("-----开始执行测试用例-----")
         url = "https://www.ygg.re/"
         sb.uc_open_with_reconnect(url, 5)
         # sb.activate_cdp_mode(url)
