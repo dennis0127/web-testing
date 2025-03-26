075e349d073bd2ef84427d6397b0419d8b40e3ff
lxd <dennis940127@gmail.com>
2025-03-26 10:32:17 +0800

    1

--- test.py
+++ test.py
@@ -6,10 +6,11 @@ from seleniumbase import SB
 def test():
     with SB(uc=True, test=True, locale_code="en", xvfb="True") as sb:
         url = "https://www.ygg.re/"
-        sb.activate_cdp_mode(url)
-        sb.sleep(2)
+        sb.uc_open_with_reconnect("https://seleniumbase.io/apps/turnstile")
+        # sb.activate_cdp_mode(url)
+        # sb.sleep(2)
         sb.uc_gui_click_captcha()
-        html_content = sb.cdp.get_page_source()
+        html_content = sb.get_page_source()
         print(html_content)
 
 
