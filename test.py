3f7acadf9d1475cd5fc3173ef97e8f5490cf4c07
lxd <dennis940127@gmail.com>
2025-03-26 10:15:33 +0800

    1

--- test.py
+++ test.py
@@ -1,6 +1,4 @@
 #!/usr/bin/env  python3
 # -*- coding: utf-8 -*-
-from seleniumbase import SB
-
-with SB(uc=True, test=True, locale_code="en", xvfb="True") as sb:
-    url = "https://www.ygg.re/"
+if __name__ == "__main__":
+    print("Hello, World!")
