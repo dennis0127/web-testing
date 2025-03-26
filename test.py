19df5b65e29b8a57716f1067956d2015a237cfe9
lxd <dennis940127@gmail.com>
2025-03-26 14:40:17 +0800

    1

--- test.py
+++ test.py
@@ -6,11 +6,15 @@ from seleniumbase import SB
 def test():
     with SB(uc=True, test=True, locale_code="en") as sb:
         print("-----开始执行测试用例-----")
-        url = "https://www.ygg.re/"
-        sb.uc_open_with_reconnect(url, 5)
-        # sb.activate_cdp_mode(url)
-        # sb.sleep(2)
+        # url = "https://vstat.info/amazon.com"
+        url = "www.planetminecraft.com/account/sign_in/"
+        # sb.uc_open_with_reconnect(url, 5)
+        sb.activate_cdp_mode(url)
+        print("-----开始执行测试用例2-----")
+        sb.sleep(3)
         sb.uc_gui_click_captcha()
+        sb.sleep(2)
+        print("-----开始执行测试用例3-----")
         html_content = sb.get_page_source()
         print(html_content)
 
