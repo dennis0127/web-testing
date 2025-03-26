5b2dfed171a0feaf08456b3e85080aef7cda2e9e
lxd <dennis940127@gmail.com>
2025-03-26 10:19:01 +0800

    1

--- test.py
+++ test.py
@@ -1,4 +1,16 @@
 #!/usr/bin/env  python3
 # -*- coding: utf-8 -*-
+from seleniumbase import SB
+
+
+def test():
+    with SB(uc=True, test=True, locale_code="en", xvfb="True") as sb:
+        url = "https://www.ygg.re/"
+        sb.activate_cdp_mode(url)
+        sb.uc_gui_click_captcha()
+        html_content = sb.cdp.get_page_source()
+        print(html_content)
+
+
 if __name__ == "__main__":
     print("Hello, World!")
