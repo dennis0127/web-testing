12a225e33053952cb94b2a0f2545043c526c45fd
lxd <dennis940127@gmail.com>
2025-03-26 10:39:45 +0800

    1

--- test.py
+++ test.py
@@ -6,7 +6,7 @@ from seleniumbase import SB
 def test():
     with SB(uc=True, test=True, locale_code="en", xvfb="True") as sb:
         url = "https://www.ygg.re/"
-        sb.uc_open_with_reconnect(url)
+        sb.uc_open_with_reconnect(url, 5)
         # sb.activate_cdp_mode(url)
         # sb.sleep(2)
         sb.uc_gui_click_captcha()
