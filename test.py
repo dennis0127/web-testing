aac6b7fb4914794c736ba2b138539fc7e88cd3c6
lxd <dennis940127@gmail.com>
2025-03-26 10:25:02 +0800

    1

--- test.py
+++ test.py
@@ -7,6 +7,7 @@ def test():
     with SB(uc=True, test=True, locale_code="en", xvfb="True") as sb:
         url = "https://www.ygg.re/"
         sb.activate_cdp_mode(url)
+        sb.sleep(2)
         sb.uc_gui_click_captcha()
         html_content = sb.cdp.get_page_source()
         print(html_content)
