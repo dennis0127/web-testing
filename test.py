fecdbf1a5de2079848a6fd2f99971473d8d5e6b6
lxd <dennis940127@gmail.com>
2025-03-26 16:43:47 +0800

    1

--- test.py
+++ test.py
@@ -13,10 +13,10 @@ def test():
     ) as sb:
         url = "https://vstat.info/amazon.com"
         # url = "https://www.ygg.re/"
-        sb.uc_open_with_reconnect(url, 5)
-        # sb.activate_cdp_mode(url)
+        # sb.uc_open_with_reconnect(url, 5)
+        sb.activate_cdp_mode(url)
+        sb.sleep(5)
         print(sb.get_page_title())
-        # sb.sleep(5)
         sb.uc_gui_click_captcha()
         print(sb.get_page_title())
         sb.sleep(2)
