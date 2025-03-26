06e1cc62bf280235b431f18ee3002969fc7fb138
lxd <dennis940127@gmail.com>
2025-03-26 09:26:52 +0800

    1

---
+++ main.py
@@ -0,0 +1,6 @@
+def main():
+    print("Hello from web-testing!")
+
+
+if __name__ == "__main__":
+    main()
--- test.py
+++ test.py
@@ -3,8 +3,10 @@
 from seleniumbase import SB
 
 with SB(uc=True, test=True, locale_code="en", xvfb="True") as sb:
-    url = "https://ipinfo.thordata.com/"
-    sb.uc_open_with_reconnect(url, 5)
+    url = "https://www.ygg.re/"
+    # sb.uc_open_with_reconnect(url, 5)
+    sb.activate_cdp_mode(url)
     sb.uc_gui_click_captcha()
-    html_content = sb.get_page_source()
+    html_content = sb.cdp.get_page_source()
     print(html_content)
+
